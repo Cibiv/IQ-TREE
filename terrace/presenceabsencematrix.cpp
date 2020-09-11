91c60f28862240cf82716a6a44c06b5d0c495d8f
Olga Chernomor <o.chernomor@gmail.com>
2020-09-11 13:38:02 +0200

    linker issue

---
+++ terrace/presenceabsencematrix.cpp
@@ -0,0 +1,8 @@
+//
+//  presenceabsencematrix.cpp
+//  iqtree
+//
+//  Created by Admin on 10.09.20.
+//
+
+#include "presenceabsencematrix.hpp"
---
+++ terrace/presenceabsencematrix.hpp
@@ -0,0 +1,13 @@
+//
+//  presenceabsencematrix.hpp
+//  iqtree
+//
+//  Created by Admin on 10.09.20.
+//
+
+#ifndef presenceabsencematrix_hpp
+#define presenceabsencematrix_hpp
+
+#include <stdio.h>
+
+#endif /* presenceabsencematrix_hpp */
